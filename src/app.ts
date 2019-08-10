9f27e803ce257a0c59b7077938731e4fe672edf7
David De Wulf <ledavid64@gmail.com>
2019-08-10 06:47:41 -0700

    setup express typescript prettier

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
