4bb39e8ad508bad58e1ce4f649d4ff956590afd4
Sahil Kalra <s1kalra@garagescript.org>
2019-08-14 20:13:38 -0700

    Fixes Merge conflicts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
-
+import path from 'path'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '../../views'))
+
+app.get('/', (req, res) => res.render('index', { message: 'Hello World' }))
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
