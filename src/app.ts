c0d6486199dc89efced209633b9ebdf9921b1a5c
Sahil Kalra <s1kalra@garagescript.org>
2019-08-15 15:05:01 -0700

    Serve admin file through ejs

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express from 'express'
 import path from 'path'
 const app = express()
 const port = process.env.PORT || 3000
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../../src/views'))
 
 app.get('/', (req, res) => res.render('index', { message: 'Hello World' }))
+
+app.get('/admin/serviceHostKeys', (req, res) => {
+  res.render('admin')
+})
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
