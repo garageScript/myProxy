6730d98814272771cf070133ffebc85fcf147698
Sahil Kalra <s1kalra@garagescript.org>
2019-08-14 19:11:46 -0700

    #12 Test UI for serviceHostKeys

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/admin/serviceHostKeys', (req, res) => {
+  res.sendFile(`${path.join(__dirname, '../../src/public/admin.html')}`)
+})
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
