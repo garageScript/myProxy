66088f1686076a6253df91d502c54bc88e2ebb3a
Sahil Kalra <s1kalra@garagescript.org>
2019-08-15 14:56:14 -0700

    Move views into source folder

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import path from 'path'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '../../views'))
+app.set('views', path.join(__dirname, '../../src/views'))
 
 app.get('/', (req, res) => res.render('index', { message: 'Hello World' }))
 
