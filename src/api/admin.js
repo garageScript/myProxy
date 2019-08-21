be5f8fec6b24e1b7289c146004ee2c15378f3116
Alberto Lopez <albertolopez7@gmail.com>
2019-08-21 09:42:45 -0700

    API updates

---
+++ src/api/admin.js
@@ -0,0 +1,91 @@
+const data = {}
+const writeFile = ()=>{
+  const fileData = JSON.stringify(data)
+  fs.writeFile('./data.db', fileData,  (err)=>{
+    if(err) {
+      return console.log('error', err)
+    }
+    console.log('file write success')
+  })
+}
+
+fs.readFile('./data.db', (err, file)=>{
+  if(err){
+    return console.log('error', err)
+  }
+  const fileData = JSON.parse(file || '{}')
+  data.serviceKeys = fileData.serviceKeys || []
+})
+
+app.post('/api/admin/serviceHostsKeys', (req, res)=>{
+  // create service keys
+  const serviceHostKeys = {id: data.serviceKeys.length, ...req.body}
+  data.serviceKeys.push(serviceHostKeys)
+  writeFile()
+  res.json(serviceHostKeys)
+})
+
+app.get('/api/admin/serviceHostsKeys', (req, res)=>{
+  // get all servicekeys
+  res.json(data)
+})
+
+app.get('/api/admin/serviceHostsKeys/:id', (req, res)=>{
+  // grab one servicekey
+  const id = req.params.id
+  let key;
+  data.serviceKeys.forEach((keys)=>{
+    if(keys[id]){
+      key = keys;
+    }
+  })
+  res.json(key)
+})
+
+app.delete('/api/admin/serviceHostsKeys/:id', (req, res)=>{
+  // delete a servicekey
+  const id = req.params.id
+  data.serviceKeys.forEach((keys, i)=>{
+    if(keys[id]){
+      data.serviceKeys.splice(i, 1) 
+    }
+  })
+  writeFile()
+  res.json(data)
+})
+
+app.put('/api/admin/serviceHostsKeys/:id', (req, res)=>{
+  // replace servicekey info
+  const id = req.params.id
+  let replacedKey;
+  data.serviceKeys.forEach((keys, i)=>{
+    if(keys[id]){
+      data.serviceKeys[i]['key']=req.body.key
+      data.serviceKeys[i]['value']=req.body.value
+      data.serviceKeys[i]['service']=req.body.service 
+      replacedKey = data.serviceKeys[i]
+    }
+  })
+  res.json(replacedKey)
+})
+
+app.patch('/api/admin/serviceHostsKeys/:id', (req, res)=>{
+  // edit servicekey info
+  const id = req.params.id
+  let editedKey;
+  data.serviceKeys.forEach((keys,i)=>{
+    if(keys[id]){
+      if(req.body.key){
+        data.serviceKeys[i]['key']=req.body.key
+      }
+      if(req.body.service){
+        data.serviceKeys[i]['service']=req.body.service
+      }
+      if(req.body.value){
+        data.serviceKeys[i]['value']=req.body.value
+      }
+      editedKey = data.serviceKeys[i]
+    }
+  })
+  res.json(editedKey)
+})
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
-const express = require('express')
-const fs = require('fs')
-const bodyParser = require('body-parser')
-const { promisify } =  require('util')
-const exec = promisify(require('child_process').exec)
-
-const app = express()
-app.use(bodyParser.json());
-const port = 3000
-
+import express from 'express'
 import path from 'path'
+import fs from 'fs'
+
 const app = express()
-const port = process.env.PORT || 3000
+const port: String | number= process.env.PORT || 3000
+const apiRouter = require('./api')
+
+app.use(express.json());
+app.use('/api', apiRouter)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../../src/views'))
 
@@ -18,49 +15,6 @@ app.get('/', (req, res) => res.render('index', { message: 'Hello World' }))
 
 app.get('/admin/serviceHostKeys', (req, res) => {
   res.render('admin')
-})
-
-let store = []
-const createCert = async (key: String, service: String, value: String)=>{
-  try{
-    const terminalCommand = await exec('cat app.ts')
-    const certStore = JSON.stringify({cert: terminalCommand})
-    store.push(certStore)
-    fs.appendFile('cert.txt', store, (error)=>console.log('error', error))
-  }catch(error){
-    console.log('error', error) 
-  }
-}
-
-app.post('/api/admin/serviceHostsKeys', (req, res)=>{
-  // create proxy
-    createCert('10','dns', 'hello')
-    res.json({success: 'proxy created'})
-})
-
-app.get('/api/admin/serviceHostsKeys', (req, res)=>{
-  // get all proxies
-  res.json({proxies: 'all proxies'})
-})
-
-app.get('/api/admin/serviceHostsKeys/:id', (req, res)=>{
-  // get one proxy
-  res.json({proxy: 'one proxy'})
-})
-
-app.delete('/api/admin/serviceHostsKeys/:id', (req, res)=>{
-  // delete proxy
-  res.json({success: 'proxy deleted'})
-})
-
-app.put('/api/admin/serviceHostsKeys/:id', (req, res)=>{
-  // replace proxy info
-  res.json({succes: 'proxy info replaced'})
-})
-
-app.patch('/api/admin/serviceHostsKeys/:id', (req, res)=>{
-  // edit proxy info
-  res.json({success: 'proxy info edited'})
 })
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
