c9433ca1382759eda9fea1d41ee6fe97ec65ded8
Alberto Lopez <albertolopez7@gmail.com>
2019-08-23 13:52:07 -0700

    added lib file to store the database information for reading and writing

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -1,76 +1,60 @@
 import { DB, ServiceKey } from './types/admin'
+import { setData, getData, data} from './lib/data'
 import express from 'express'
 import fs from 'fs'
 
 const app = express.Router()
-const data: DB = {
-  serviceKeys: []
-}
 
-const writeFile = ()=>{
-  const fileData: string = JSON.stringify(data)
-  fs.writeFile('./data.db', fileData,  (err)=>{
-    if(err) {
-      return console.log('error', err)
-    }
-    console.log('file write success')
-  })
-}
+getData('servicekeys')
 
-fs.readFile('./data.db', (err, file)=>{
-  if(err){
-    return console.log('error', err)
-  }
-  console.log('file read success')
-  const fileData: DB = JSON.parse(file.toString() || '{}')
-  data.serviceKeys = fileData.serviceKeys || []
-})
-
-app.post('/serviceHostsKeys', (req, res)=>{
+app.post('/serviceHostsKey', (req, res) => {
   // create service keys
-  const serviceHostKeys: ServiceKey = {id: data.serviceKeys.length, ...req.body}
+  const serviceHostKeys: ServiceKey = {
+    id: data.serviceKeys.length,
+    ...req.body
+  }
   data.serviceKeys.push(serviceHostKeys)
-  writeFile()
+  setData('servicekeys', data)
   res.json(serviceHostKeys)
 })
 
-app.get('/serviceHostsKeys', (req, res)=>{
+app.get('/serviceHostsKey', (req, res) => {
   // get all servicekeys
   res.json(data.serviceKeys)
 })
 
-app.get('/serviceHostsKeys/:id', (req, res)=>{
+app.get('/serviceHostsKey/:id', (req, res) => {
   // grab one servicekey
   const id: number = Number(req.params.id)
   const selectedKey: ServiceKey = data.serviceKeys[id]
   res.json(selectedKey)
 })
 
-app.delete('/serviceHostsKeys/:id', (req, res)=>{
+app.delete('/serviceHostsKey/:id', (req, res) => {
   // delete a servicekey
   const id: number = Number(req.params.id)
-  data.serviceKeys.splice(id, 1) 
-  writeFile()
+  data.serviceKeys.splice(id, 1)
+  setData('servicekeys', data)
   res.json(data.serviceKeys)
 })
 
-app.put('/serviceHostsKeys/:id', (req, res)=>{
+app.put('/serviceHostsKey/:id', (req, res) => {
   // replace servicekey info
   const id: number = Number(req.params.id)
-  data.serviceKeys[id] = {...req.body}
+  data.serviceKeys[id] = { ...req.body }
   const replacedKey: ServiceKey = data.serviceKeys[id]
-  writeFile()
+  setData('servicekeys', data)
   res.json(replacedKey)
 })
 
-app.patch('/serviceHostsKeys/:id', (req, res)=>{
+app.patch('/serviceHostsKey/:id', (req, res) => {
   // edit servicekey info
   const id: number = Number(req.params.id)
-      if(req.body.key) data.serviceKeys[id]['key']=req.body.key
-      if(req.body.service)data.serviceKeys[id]['service']=req.body.service
-      if(req.body.value) data.serviceKeys[id]['value']=req.body.value
-      const editedKey: ServiceKey = data.serviceKeys[id]
-  writeFile()
+  if (req.body.key) data.serviceKeys[id]['key'] = req.body.key
+  if (req.body.service) data.serviceKeys[id]['service'] = req.body.service
+  if (req.body.value) data.serviceKeys[id]['value'] = req.body.value
+  const editedKey: ServiceKey = data.serviceKeys[id]
+  setData('servicekeys', data)
   res.json(editedKey)
 })
 
---
+++ src/api/lib/data.ts
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import { DB, ServiceKey } from '../types/admin'
+
+const data: DB = {
+  serviceKeys: []
+}
+
+const getData = (serviceKeys: string)=>{
+  if(serviceKeys){
+    fs.readFile('./data.db', (err, file) => {
+      if (err) {
+        return console.log('error', err)
+      }
+      console.log('file read success')
+      const fileData: DB = JSON.parse(file.toString() || '{}')
+      data.serviceKeys = fileData.serviceKeys || []
+    })
+  }
+}
+
+const setData = (serviceKeys: string, data: DB)=>{
+  if(serviceKeys && data){
+    const fileData: string = JSON.stringify(data)
+    fs.writeFile('./data.db', fileData, err => {
+      if (err) {
+        return console.log('error', err)
+      }
+      console.log('file write success')
+    })
+  }
+}
+
+export {
+  getData, setData, data
+}
