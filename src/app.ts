67dfad899b48f57f5f03be92ea3a03077f593869
David De Wulf <ledavid64@gmail.com>
2019-08-24 02:26:42 -0700

    putback admin midleware...^^

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.urlencoded())
 app.use(cookieParser())
-// app.use('/admin', adminRouter)
+app.use('/admin', adminRouter)
 app.use('/services', services)
 
 app.set('view engine', 'ejs')
