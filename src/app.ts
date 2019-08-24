f86e758c9b85865586206147fe37dfce59dc6dcf
David De Wulf <ledavid64@gmail.com>
2019-08-24 02:24:20 -0700

    Refactor and split code

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,18 @@
 import express from 'express'
 import path from 'path'
-import https from 'https'
 import cookieParser from 'cookie-parser'
 import { adminRouter } from './admin/index'
+import { services } from './services'
 const app = express()
 const port = process.env.PORT || 3000
 
-// godaddy API_FROM_DB (JSON)
-// KEY and SECRET are on server within README.md
-const service = 'api.godaddy.com'
-const key = process.env.KEY
-const secret = process.env.SECRET
-const options = {
-  host: service,
-  path: '/v1/domains?statuses=ACTIVE',
-  headers: { Authorization: `sso-key ${key}:${secret}` }
-}
-
 app.use(express.urlencoded())
 app.use(cookieParser())
-app.use('/admin', adminRouter)
+// app.use('/admin', adminRouter)
+app.use('/services', services)
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../../src/views'))
-
-app.get('/admin/domains', (_, res) => {
-  // List of domains will be sent to the frontend (JSON)
-  https
-    .request(options, response => {
-      response.on('data', data => res.json(JSON.parse(data)))
-    })
-    .on('error', err => res.json(JSON.parse(err.message)))
-    .end()
-})
 
 app.get('/', (req, res) => res.render('client'))
 
