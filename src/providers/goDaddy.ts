807b831ea29159396218a4a173c5f197909f2f4a
David De Wulf <ledavid64@gmail.com>
2019-08-26 01:16:33 -0700

    Get Key from DB

--- src/providers/goDaddy.ts
+++ src/providers/goDaddy.ts
@@ -1,7 +1,8 @@
 import fetch from '../helpers/httpRequest'
+import { getData } from '../api/lib/data'
 
-const KEY = process.env.API_KEY_GO_DADDY
-const SECRET = process.env.SECRET_GO_DADDY
+const KEY = getData('serviceKeys').key
+const SECRET = getData('serviceKeys').secret
 const SERVICE = 'https://api.godaddy.com'
 const ACTIVE_DOMAINS = `${SERVICE}/v1/domains?statuses=ACTIVE`
 const OPTIONS = { headers: { Authorization: `sso-key ${KEY}:${SECRET}` } }
