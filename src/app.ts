9d4557ad125e222310cbb985a33fab5e137b0f83
Sahil Kalra <s1kalra@garagescript.org>
2019-08-25 19:21:24 -0700

    Removes express.static

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,6 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/admin', adminRouter)
 app.use('/api', apiRouter)
-app.use(express.static(`${__dirname}`))
 
 app.set('view engine', 'ejs')
 
