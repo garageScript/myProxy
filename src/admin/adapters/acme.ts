478c44b2d31d9fc98b66965c94586d8b88f9ac0e
Alberto Lopez <albertolopez7@gmail.com>
2019-08-27 17:05:56 -0700

    executable child-process added to support terminal commands

---
+++ src/admin/adapters/acme.ts
@@ -0,0 +1,22 @@
+import util from 'util'
+import cp from 'child_process'
+import express from 'express'
+
+const app = express.Router()
+const exec = util.promisify(cp.exec)
+
+const gdKey = '123'
+const gdSecret = '456'
+
+app.post('/sslCerts', async (req, res) => {
+  try{
+    const { stdout, stderr } = await exec(`GD_Key=${gdKey} GD_Secret=${gdSecret} ./acme.sh/acme.sh --issue --dns dns_gd -d dummydomain.com -d www.dummydomain.com`)
+    if(stderr){
+      console.log('stderr', stderr)
+    }
+    console.log('stdout', stdout)
+  }catch(err){
+    console.log('failed to create cert', err)
+  }
+  res.json('cert successfully created')
+})
