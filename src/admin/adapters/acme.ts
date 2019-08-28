0fa696ee0fc468dc0440fbe834640784932fcc62
Alberto Lopez <albertolopez7@gmail.com>
2019-08-27 17:08:21 -0700

    updated formatting

--- src/admin/adapters/acme.ts
+++ src/admin/adapters/acme.ts
@@ -9,13 +9,15 @@ const gdKey = '123'
 const gdSecret = '456'
 
 app.post('/sslCerts', async (req, res) => {
-  try{
-    const { stdout, stderr } = await exec(`GD_Key=${gdKey} GD_Secret=${gdSecret} ./acme.sh/acme.sh --issue --dns dns_gd -d dummydomain.com -d www.dummydomain.com`)
-    if(stderr){
+  try {
+    const { stdout, stderr } = await exec(
+      `GD_Key=${gdKey} GD_Secret=${gdSecret} ./acme.sh/acme.sh --issue --dns dns_gd -d dummydomain.com -d www.dummydomain.com`
+    )
+    if (stderr) {
       console.log('stderr', stderr)
     }
     console.log('stdout', stdout)
-  }catch(err){
+  } catch (err) {
     console.log('failed to create cert', err)
   }
   res.json('cert successfully created')
