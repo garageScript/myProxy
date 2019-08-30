24d2a98fa6c0a0678c25779ed66a762a819b0c4d
Alberto Lopez <albertolopez7@gmail.com>
2019-08-30 09:06:12 -0700

    prettier made changes

--- src/api/serviceConfig.ts
+++ src/api/serviceConfig.ts
@@ -1,6 +1,6 @@
 /*eslint @typescript-eslint/camelcase: ["error", {properties: "never"}]*/
 const serviceConfig: any = {
-  'dns_gd': {
+  dns_gd: {
     name: 'Godaddy',
     keys: ['GD_Key', 'GD_Secret']
   }
