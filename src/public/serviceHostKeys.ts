d0f52e935b5370b71e20a30b016a8c6a739a819e
Sahil Kalra <s1kalra@garagescript.org>
2019-08-29 21:12:37 -0700

    New Mock UI Admin ServiceHostKeys

--- src/public/serviceHostKeys.ts
+++ src/public/serviceHostKeys.ts
@@ -2,7 +2,21 @@ const serviceKey = document.querySelector('#serviceKey') as HTMLInputElement
 const serviceValue = document.querySelector('#value') as HTMLInputElement
 const service = document.querySelector('#service') as HTMLInputElement
 const submit = document.querySelector('#newService') as HTMLElement
-submit.onclick = (): boolean => {
+const serviceKeysList = document.querySelector('#serviceHostKeys') as HTMLElement
+
+/* HardCoded dns object to display mock UI */
+const temp = {
+  dnsGd: {
+    name: "GoDaddy",
+    keys: ["GD_Key", "GD_Secret"]
+  },
+  dnsPdns: {
+    name: "PowerDNS",
+    keys: ["PDNS_Url", "PDNS_ServerId", "PDNS_Token", "PDNS_Ttl"]
+  }
+} 
+  /*submit.onclick = (): boolean => {
+  if(!serviceKey.value || !serviceValue.value || !service.value) return false
   fetch('/api/admin/serviceHostKeys', {
     method: 'POST',
     body: JSON.stringify({
@@ -14,5 +28,25 @@ submit.onclick = (): boolean => {
       'Content-Type': 'application/json'
     }
   })
+  serviceKey.value = ''
+  serviceValue.value = ''
+  service.value = ''
   return false
-}
+}*/
+serviceKeysList.innerHTML = Object.entries(temp).reduce((acc, [service, info]) => {
+  const keyInputs = info.keys.reduce((acc, key) => {
+    return acc + `
+      <div class="enteredKey">
+        ${key}
+        <input type="text" value=""></input>
+      </div>     
+    `
+  }, '')
+  return acc + `
+    <h4>${info.name}</h4>
+    <li class="list-group-item">
+      ${keyInputs}
+      <button type="button" class="btn btn-primary">Create</button>
+    </li>
+  `
+}, '')
