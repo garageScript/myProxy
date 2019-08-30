ba7c488976af5cd0ca5676b5e8fb315f9bfa37a3
David De Wulf <ledavid64@gmail.com>
2019-08-29 18:42:55 -0700

    Add a better type with TS

--- src/providers/goDaddy.ts
+++ src/providers/goDaddy.ts
@@ -1,13 +1,14 @@
 import fetch from '../helpers/httpRequest'
 import { getData } from '../api/lib/data'
+import { ServiceKey } from '../api/types/admin'
 
 export const getDomains = () => {
   const serviceKeys: any = getData('serviceKeys')
   const { service, value: key } = serviceKeys.find(
-    (el: { key: string }) => el.key === 'GD_Key'
-  )
+    ((el: ServiceKey ) => el.key === 'GD_Key'
+  ))
   const { value: secret } = serviceKeys.find(
-    (el: { key: string }) => el.key === 'GD_Secret'
+    (el: ServiceKey) => el.key === 'GD_Secret'
   )
   const url = `${service}/v1/domains?statuses=ACTIVE`
   const options = { headers: { Authorization: `sso-key ${key}:${secret}` } }
