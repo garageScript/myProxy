b61018bad17c7f6e6e10c160cf8a6cfbb57248d8
Rahul Kalra <noob101@garagescript.org>
2019-09-03 20:23:49 -0700

    Fix #64 UI for POST api/mappings

--- src/api/mapping.ts
+++ src/api/mapping.ts
@@ -1,20 +1,21 @@
 import express from 'express'
 import uuid4 from 'uuid/v4'
 import { setData, getData, getMappings } from './lib/data'
-import { Mapping } from './types/general'
+import { Mapping } from './types/general' 
 const mappingRouter = express.Router()
 
-mappingRouter.post('/', (req, res) => {
+mappingRouter.post('/', (req, res) =>{
   const domainKeys = getMappings()
   const mappingObject: Mapping = {
-    domain: req.body.domain,
-    port: req.body.port,
-    ip: req.body.ip,
-    id: uuid4()
+    'domain': req.body.domain,
+    'port' : req.body.portValue,
+    'ip' : req.body.ip,
+    'id' : uuid4()
   }
   domainKeys.push(mappingObject)
   setData('mappings', domainKeys)
   res.json(mappingObject)
 })
 
-export default mappingRouter
+
+export default mappingRouter 
