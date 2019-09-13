0e15ee82144f50805dca807d6b698a9db4c4ed56
Alberto Lopez <albertolopez7@gmail.com>
2019-09-13 23:09:58 +0000

    added data deleted by commit

--- src/app.ts
+++ src/app.ts
@@ -45,10 +45,14 @@ app.post('/login', (req, res) => {
 })
 
 const listener = (): void => {
+  const cyan = '\x1b[36m\u001b[1m%s\x1b[0m'
+  const red = '\x1b[31m\u001b[1m%s\x1b[0m'
   if (!process.env.ADMIN) {
-    return console.log('Admin UI/API is turned off')
+    return console.log('red', 'Admin UI/API is turned off')
   }
-  app.listen(port, () => console.log(`app listening on port ${port}!`))
+  app.listen(port, () =>
+      console.log(cyan, `myProxy is running on port ${port}!`)
+    )
 }
 
 const server = https.createServer(
