0202209a9bdc799377d92cbd62d259f4ce5bc891
Sahil Kalra <s1kalra@garagescript.org>
2019-09-13 15:01:36 -0700

    Fixes syntax issues

--- src/api/mapping.ts
+++ src/api/mapping.ts
@@ -1,3 +1,4 @@
+/* eslint @typescript-eslint/camelcase: 0 */
 import express from 'express'
 import uuid4 from 'uuid/v4'
 import util from 'util'
@@ -19,19 +20,40 @@ mappingRouter.post('/', (req, res) => {
   }
   domainKeys.push(mappingObject)
   setData('mappings', domainKeys)
-  const projectFolder = `${path.join(__dirname, '../../projects')}/${req.body
-    .domain + req.body.subDomain}`
+  const projectFolder = `${req.body.domain}`
+  const prodConfig = {
+    apps: [
+      {
+        name: 'myProxy-prod',
+        script: './build/app.js',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env_production: {
+          NODE_ENV: 'production',
+          PORT: parseInt(req.body.port, 10),
+          ADMIN: '123'
+        }
+      }
+    ]
+  }
   exec(`
-    mkdir -p ../../projects
-    mkdir ${projectFolder}
-    git init ${projectFolder}
-    cd ${projectFolder} 
-    cp ../../scripts/post-receive .git/hooks/
-    touch README.md
+    mkdir -p ~/projects
+    mkdir ~/projects/${projectFolder}
+    git init ~/projects/${projectFolder}
+    cp ${path.join(
+    __dirname,
+    '../../scripts'
+  )}/post-receive ~/projects/${projectFolder}/.git/hooks/
+    cd ~/projects/${projectFolder}
+    echo "module.exports =" > deploy.config.js
+    echo '${JSON.stringify(prodConfig)}' >> deploy.config.js
     git add .
     git commit -m "Initial Commit"
-    git checkout -b prod`)
-  res.json(mappingObject)
+    git checkout -b prod`).then(() => {
+    res.json(mappingObject)
+  })
 })
 
 mappingRouter.get('/', (req, res) => {
