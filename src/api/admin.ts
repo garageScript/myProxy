913e3e09fb6ad5411b036c3719302be60d19959a
SahilKalra98 <sahilkalra4321@gmail.com>
2019-09-20 19:47:53 -0700

    Removes comments

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -21,7 +21,6 @@ app.post('/sslCerts', async (req, res) => {
   try {
     const serviceKeys = getProviderKeys().filter(d => d.service === service)
     const { keys } = serviceConfig[service]
-      /*
     const envVars = keys.reduce((acc: string, key: string) => {
       const { value } = serviceKeys.find(d => d.key === key) || { value: '' }
       return acc + `${key}=${value} `
@@ -39,7 +38,7 @@ app.post('/sslCerts', async (req, res) => {
           : JSON.stringify(cert1Response.stderr)
       }`
       return res.json(serviceResponse)
-    }*/
+    }
 
     const { stdout: ipaddress } = await exec('curl ifconfig.me')
     const providerService = providers[service] as ProviderService
