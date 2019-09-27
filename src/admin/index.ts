cd5dfbd1c4531e67338a15048704b3bc8def9631
Song Zheng <sozheng@paypal.com>
2019-09-27 02:19:35 -0700

    api fixes

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,9 +1,7 @@
 import express from 'express'
-import { auth } from '../auth'
 
 const adminRouter = express.Router()
 
-adminRouter.use(auth)
 adminRouter.get('/', (req, res) => {
   res.render('admin/providers')
 })
--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,20 +2,26 @@ import express from 'express'
 import { hashPass } from '../helpers/crypto'
 const auth = express.Router()
 
-const isCorrectCredentials = (password: string): boolean => {
-  const adminPassword = hashPass(process.env.ADMIN as string)
-  const userPassword = hashPass(password)
+const isCorrectCredentials = (password: string, correct: string): boolean => {
+  const adminPassword = hashPass(password)
+  const userPassword = hashPass(correct)
   return userPassword === adminPassword
 }
 
-auth.use('/*', (req, res, next) => {
-  const { adminPass } = req.cookies
-  if (
-    !adminPass &&
-    !isCorrectCredentials((req.headers.Authorization as string) || '')
-  )
-    return res.status(401).redirect('/login')
-  next()
-})
+const setupAuth = password => {
+  return (req, res, next) => {
+    const { adminPass } = req.cookies
+    if (
+      !adminPass &&
+      !isCorrectCredentials(
+        (req.headers.authorization as string) || '',
+        password
+      )
+    ) {
+      return res.status(401).send('Unauthorized')
+    }
+    next()
+  }
+}
 
-export { auth, isCorrectCredentials }
+export { setupAuth, isCorrectCredentials }
