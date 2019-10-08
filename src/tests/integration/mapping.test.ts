2306617ca9529ec5f872cc85d923935f4689c9df
Alberto Lopez <albertolopez7@gmail.com>
2019-10-08 00:53:14 -0700

    delete integration test refactored

--- src/tests/integration/mapping.test.ts
+++ src/tests/integration/mapping.test.ts
@@ -2,8 +2,8 @@ import { startAppServer } from '../../server/server'
 import fetch from 'node-fetch'
 import uuidv4 from 'uuid/v4' 
 
-const TEST_PORT = process.env.PORT || 3312 
-const ADMIN = process.env.ADMIN || 'hello'
+const TEST_PORT = process.env.PORT || 50604
+const ADMIN = process.env.ADMIN || 'hjhj'
 const apiURL = `http://127.0.0.1:${TEST_PORT}`
 
 describe('/api', () =>{
@@ -55,15 +55,8 @@ describe('/api', () =>{
         domain,
         subDomain,
         port,
-
       })
-    }).then(r => r.json()).then((data)=>{
-      expect(data.port).toEqual(port)
-      expect(data.subDomain).toEqual(subDomain)
-      expect(data.domain).toEqual(domain)
-      expect(data.fullDomain).toEqual(`${subDomain}.${domain}`)
-      return data
-    }).then(async (data)=>{
+    }).then(r => r.json()).then(async (data)=>{
       await fetch(`${apiURL}/api/mappings/delete/${data.id}`, {
         method: 'DELETE', 
         headers: {
@@ -71,6 +64,10 @@ describe('/api', () =>{
           'Content-Type': 'application/json'
         }
       }).then(r=>r.json()).then((del)=>{
+        expect(data.port).toEqual(port)
+        expect(data.subDomain).toEqual(subDomain)
+        expect(data.domain).toEqual(domain)
+        expect(data.fullDomain).toEqual(`${subDomain}.${domain}`)
         expect(del.id).toEqual(data.id)
       })
     })  
