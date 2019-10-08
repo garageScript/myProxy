0b263cf1a28ba4f463e7f026bbb6d2573871b76f
Alberto Lopez <albertolopez7@gmail.com>
2019-10-07 23:31:53 -0700

    Integration test: delete mapping

--- src/tests/integration/mapping.test.ts
+++ src/tests/integration/mapping.test.ts
@@ -2,8 +2,8 @@ import { startAppServer } from '../../server/server'
 import fetch from 'node-fetch'
 import uuidv4 from 'uuid/v4' 
 
-const TEST_PORT = process.env.PORT || 50604
-const ADMIN = process.env.ADMIN || 'hjhj'
+const TEST_PORT = process.env.PORT || 3312 
+const ADMIN = process.env.ADMIN || 'hello'
 const apiURL = `http://127.0.0.1:${TEST_PORT}`
 
 describe('/api', () =>{
@@ -16,8 +16,6 @@ describe('/api', () =>{
   afterAll(()=>{
     server.close()
   })
-
-
 
   it('checks mappings for newly added mapping', async()=>{
     const subDomain = `testing${uuidv4()}`
@@ -41,5 +39,40 @@ describe('/api', () =>{
       expect(data.domain).toEqual(domain)
       expect(data.fullDomain).toEqual(`${subDomain}.${domain}`)
     })
+  })
+
+  it('Delete mapping', async()=>{
+    const subDomain = `delete${uuidv4()}`
+    const domain = 'albertow'
+    const port = '4500'
+    await fetch(`${apiURL}/api/mappings`, {
+      method: 'POST', 
+      headers: {
+        authorization: ADMIN,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        domain,
+        subDomain,
+        port,
+
+      })
+    }).then(r => r.json()).then((data)=>{
+      expect(data.port).toEqual(port)
+      expect(data.subDomain).toEqual(subDomain)
+      expect(data.domain).toEqual(domain)
+      expect(data.fullDomain).toEqual(`${subDomain}.${domain}`)
+      return data
+      }).then(async (data)=>{
+        await fetch(`${apiURL}/api/mappings/delete/${data.id}`, {
+        method: 'DELETE', 
+        headers: {
+        authorization: ADMIN,
+        'Content-Type': 'application/json'
+        }
+      }).then(r=>r.json()).then((del)=>{
+        expect(del.id).toEqual(data.id)
+      })
+      })  
   })
 })
