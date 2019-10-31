dfa01379bf3024e6ee35196b7e0664437bef19c8
hwong0305 <hwong.0305@gmail.com>
2019-10-30 19:10:01 -0700

    added missing not character in bash script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if command node -v &>/dev/null; then
+if ! command node -v &>/dev/null; then
   sudo apt-get install curl
   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
   sudo apt-get install -y nodejs
