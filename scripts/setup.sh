318905f654adbc17296a1817cad2ba96a68ce7ca
hwong0305 <hwong.0305@gmail.com>
2019-10-30 18:57:03 -0700

    Updated Readme

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ if command node -v &>/dev/null; then
   sudo apt-get install curl
   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
   sudo apt-get install -y nodejs
-fi`
+fi
 npm install
 npm install pm2 -g
 if [ ! -d "./acme.sh" ] ; then
