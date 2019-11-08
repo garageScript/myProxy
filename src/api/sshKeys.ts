1d2ac8c9df32288412fc6c391b5f4cf93783d475
rkalra247 <rkalra247@gmail.com>
2019-11-07 21:30:55 -0800

    updates fix #287 by refactoring the get request for sshKeys

--- src/api/sshKeys.ts
+++ src/api/sshKeys.ts
@@ -1,13 +1,19 @@
 import express from 'express'
 import fs from 'fs'
+import os from 'os'
+const userHomeDirectory = os.homedir()
+console.log('userHomeDirectory:', userHomeDirectory)
 const sshKeyRouter = express.Router()
 
+if (!fs.existsSync(`${userHomeDirectory}/.ssh`)) {
+  fs.mkdirSync(`${userHomeDirectory}/.ssh`, { recursive: true }) 
+}
+
 sshKeyRouter.get('/', (req, res) => {
-  //res.json([])
-  fs.readFile('/home/noob101/.ssh/authorized_keys', (error, data) => {
-    console.log('error:', error)
-    console.log('data:', data.toString().split('\n'))
-    res.json(data.toString().split('\n'))
+  fs.readFile(`${userHomeDirectory}/.ssh/authorized_keys`, (error, data) => {
+    if(error||!data || !data.toString) return res.json([])
+    const keysArray = data.toString().split('\n').filter((e) =>{return e.length > 2})
+    res.json(keysArray)
   })
 })
 
