ed684260aa16d98e54b57ff19ff10765b4a9109e
hwong0305 <hwong.0305@gmail.com>
2019-11-08 20:20:28 -0800

    Fixed file with npm run autofix

--- src/api/sshKeys.ts
+++ src/api/sshKeys.ts
@@ -6,13 +6,18 @@ console.log('userHomeDirectory:', userHomeDirectory)
 const sshKeyRouter = express.Router()
 
 if (!fs.existsSync(`${userHomeDirectory}/.ssh`)) {
-  fs.mkdirSync(`${userHomeDirectory}/.ssh`, { recursive: true }) 
+  fs.mkdirSync(`${userHomeDirectory}/.ssh`, { recursive: true })
 }
 
 sshKeyRouter.get('/', (req, res) => {
   fs.readFile(`${userHomeDirectory}/.ssh/authorized_keys`, (error, data) => {
-    if(error||!data || !data.toString) return res.json([])
-    const keysArray = data.toString().split('\n').filter((e) =>{return e.length > 2})
+    if (error || !data || !data.toString) return res.json([])
+    const keysArray = data
+      .toString()
+      .split('\n')
+      .filter(e => {
+        return e.length > 2
+      })
     res.json(keysArray)
   })
 })
