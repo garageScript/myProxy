f1701464212581bbc725b7265c68be5a1914d5ae
hwong0305 <hwong.0305@gmail.com>
2019-11-28 17:09:43 -0800

    Added Router to get pm2 status from server

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import adminRouter from './admin'
 import mappingRouter from './mapping'
 import sshKeyRouter from './sshKeys'
+import statusRouter from './serversStatus'
 import { getAvailableDomains } from '../lib/data'
 
 const apiRouter = express.Router()
@@ -9,6 +10,7 @@ const apiRouter = express.Router()
 apiRouter.use('/admin', adminRouter.app)
 apiRouter.use('/mappings', mappingRouter)
 apiRouter.use('/sshKeys', sshKeyRouter)
+apiRouter.use('/status', statusRouter)
 
 apiRouter.get('/availableDomains', (req, res) => {
   const domains = getAvailableDomains()
---
+++ src/api/serversStatus.ts
@@ -0,0 +1,17 @@
+import cp from 'child_process'
+import express from 'express'
+import util from 'util'
+
+const statusRouter = express.Router()
+const exec = util.promisify(cp.exec)
+
+statusRouter.get('/', async (req, res) => {
+  try {
+    const data = await exec('su myproxy -c "pm2 jlist"')
+    res.send(data)
+  } catch (err) {
+    res.status(500).send({ err })
+  }
+})
+
+export default statusRouter
