0b3fb5800a2cecb868f3fa839a0f98dcedda7246
hwong0305 <hwong.0305@gmail.com>
2019-11-30 17:01:38 -0800

    Added test for getting server status

--- src/api/serversStatus.ts
+++ src/api/serversStatus.ts
@@ -1,14 +1,20 @@
 import cp from 'child_process'
 import express from 'express'
+import environment from '../helpers/environment'
 import util from 'util'
 
 const statusRouter = express.Router()
 const exec = util.promisify(cp.exec)
+const { isProduction } = environment
 
 statusRouter.get('/', async (req, res) => {
   try {
-    const data = await exec('su - myproxy -c "pm2 jlist"')
-    res.json(data)
+    if (isProduction()) {
+      const data = await exec('su - myproxy -c "pm2 jlist"')
+      res.json(data)
+    } else {
+      res.json({ stdout: [] })
+    }
   } catch (err) {
     res.status(500).send({ err })
   }
---
+++ src/tests/integration/status.test.ts
@@ -0,0 +1,32 @@
+import { startAppServer } from '../../server/server'
+import fetch from 'node-fetch'
+
+const TEST_PORT = process.env.PORT || 5002
+const ADMIN = process.env.ADMIN || '123'
+
+const reqHeaders = {
+  authorization: ADMIN,
+  'Content-Type': 'application/json'
+}
+
+describe('api/status', () => {
+  let server
+
+  beforeAll(async () => {
+    server = await startAppServer(TEST_PORT, ADMIN)
+  })
+
+  afterAll(() => {
+    server.close()
+  })
+
+  it('Server should return empty array for test run', async () => {
+    const res = await fetch(`http://localhost:${TEST_PORT}/api/status`, {
+      headers: reqHeaders
+    })
+
+    const data = await res.json()
+    expect(data).toStrictEqual({ stdout: [] })
+    expect(res.status).toEqual(200)
+  })
+})
