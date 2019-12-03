72f54f1f87748ecdf92f2e85c855769af43f40e1
hwong0305 <hwong.0305@gmail.com>
2019-12-03 09:48:09 -0800

    Updated Status Test to check for {} instead of []

--- src/tests/integration/status.test.ts
+++ src/tests/integration/status.test.ts
@@ -26,7 +26,7 @@ describe('api/status', () => {
     })
 
     const data = await res.json()
-    expect(data).toStrictEqual([])
+    expect(data).toStrictEqual({})
     expect(res.status).toEqual(200)
   })
 })
