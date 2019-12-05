3ceb0458ef929451f6f92b6ba19abee1a6cc3880
hwong0305 <hwong.0305@gmail.com>
2019-12-04 21:36:46 -0800

    renamed serversStatus to serverStatus

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import adminRouter from './admin'
 import mappingRouter from './mapping'
 import sshKeyRouter from './sshKeys'
-import statusRouter from './serversStatus'
+import statusRouter from './serverStatus'
 import { getAvailableDomains } from '../lib/data'
 
 const apiRouter = express.Router()
