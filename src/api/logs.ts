c1455527abb65e7a4eb239a2de4e7cf51e5cc4bb
hwong0305 <hwong.0305@gmail.com>
2019-12-09 23:40:57 -0800

    Updated deploy.config.js & added way to get output log in addition to error log

--- src/api/logs.ts
+++ src/api/logs.ts
@@ -4,28 +4,20 @@ import { getMappingByDomain } from '../lib/data'
 
 const logsRouter = express.Router()
 
-logsRouter.get('/:domain', (req, res) => {
+logsRouter.get('/:domain/err', (req, res) => {
   const { domain } = req.params
   const { fullDomain } = getMappingByDomain(domain)
 
   res.setHeader('content-type', 'text/plain')
-  fs.readdir('/home/myproxy/.pm2/logs', (err, files) => {
-    if (err) return res.status(500).send({ error: err })
-    const re = new RegExp(`^${fullDomain}-error-\\d.log`, 'g')
-    const matches = files.filter(file => file.match(re))
-    /*
-        PM2 logs file as domain-error-#.log. When the user
-        first creates the domain it would be domain-error-0.log.
-        However, when the user deletes the app and recreate the app
-        using the same name, the error log would be domain-error-1.log
-        The code below always ensures the current log file is being 
-        fetched.
-    */
-    const file = matches.reduce((latest, current) =>
-      latest < current ? current : latest
-    )
-    fs.createReadStream(`/home/myproxy/.pm2/logs/${file}`).pipe(res)
-  })
+  fs.createReadStream(`/home/myproxy/.pm2/logs/${fullDomain}-err.log`).pipe(res)
+})
+
+logsRouter.get('/:domain/out', (req, res) => {
+  const { domain } = req.params
+  const { fullDomain } = getMappingByDomain(domain)
+
+  res.setHeader('content-type', 'text/plain')
+  fs.createReadStream(`/home/myproxy/.pm2/logs/${fullDomain}-out.log`).pipe(res)
 })
 
 export default logsRouter
