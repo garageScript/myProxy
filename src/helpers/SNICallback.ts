317b5452f9141c351fbc95a19e7bb73168289412
hwong0305 <hwong.0305@gmail.com>
2019-12-11 02:13:35 -0800

    completed memory cache for SNICallback

--- src/helpers/SNICallback.ts
+++ src/helpers/SNICallback.ts
@@ -9,8 +9,8 @@ import environment from '../helpers/environment'
 const { HOME } = environment
 const acmePath = `${HOME}/.acme.sh`
 const readFileAsync = util.promisify(fs.readFile)
-let key
-let cert
+const keyCache = {}
+const certCache = {}
 
 const readFile = async (path: string) => {
   const data = await readFileAsync(path, 'utf8')
@@ -22,11 +22,15 @@ const SNICallback = async (host, cb) => {
   const [TLD, SLD, ...subDomains] = host.split('.').reverse()
   const domain = `${SLD}.${TLD}`
   const wildstar = subDomains.length > 0 ? '*.' : ''
+  const key = `${acmePath}${wildstar}${domain}`
   const keyPath = `${acmePath}/${wildstar}${domain}/${wildstar}${domain}\.key`
   const certPath = `${acmePath}/${wildstar}${domain}/fullchain.cer`
-  if (!key) key = await readFile(keyPath)
-  if (!cert) cert = await readFile(certPath)
-  const secureContext = tls.createSecureContext({ key, cert })
+  if (!keyCache[key]) keyCache[key] = await readFile(keyPath)
+  if (!certCache[key]) certCache[key] = await readFile(certPath)
+  const secureContext = tls.createSecureContext({
+    key: keyCache[key],
+    cert: certCache[key]
+  })
   if (cb) return cb(null, secureContext)
   return secureContext
 }
