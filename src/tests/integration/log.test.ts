71fd3be124d6508d3a1a836c019efcc94a1e0bb9
hwong0305 <hwong.0305@gmail.com>
2019-12-13 19:47:42 -0800

    Added test for delete logs endpoint

--- src/tests/integration/log.test.ts
+++ src/tests/integration/log.test.ts
@@ -1,4 +1,5 @@
 import { startAppServer } from '../../server/server'
+import uuidv4 from 'uuid/v4'
 import { logAdapter } from '../helpers/logAdapter'
 
 const TEST_PORT = process.env.PORT || 50608
@@ -26,6 +27,12 @@ describe('/api/logs', () => {
     const subDomain = 'Luke'
     const domain = 'Walker'
     const logResponse = await logAdapter(`/err/${subDomain}.${domain}`, 'GET')
+    expect(logResponse.status).toEqual(200)
+  })
+
+  it('checks the delete endpoint exists', async () => {
+    const fullDomain = `${uuidv4()}.walker.com`
+    const logResponse = await logAdapter(`/${fullDomain}`, 'DELETE')
     expect(logResponse.status).toEqual(200)
   })
 })
