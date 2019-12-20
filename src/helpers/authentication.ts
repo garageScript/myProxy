769403b359cf5a098980166e00a3e1e5e203be1e
hwong0305 <hwong.0305@gmail.com>
2019-12-19 17:32:03 -0800

    Refactored middleware

--- src/helpers/authentication.ts
+++ src/helpers/authentication.ts
@@ -1,16 +1,6 @@
 const validUIAccess = (req, res, next): void => {
-  if (!req.admin && !req.user) return res.redirect('/login')
+  if (!req.user.isUser) return res.redirect('/login')
   return next()
 }
 
-const validAdmin = (req, res, next): void => {
-  if (!req.admin) return res.status(401).send('Unauthorized')
-  return next()
-}
-
-const validUser = (req, res, next): void => {
-  if (!req.admin && !req.user) return res.status(401).send('Unauthorized')
-  return next()
-}
-
-export { validUIAccess, validAdmin, validUser }
+export { validUIAccess }
