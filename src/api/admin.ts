bb22e66c0287e3a3a7738a190cf2db4afd34847b
David De Wulf <ledavid64@gmail.com>
2019-12-21 15:15:38 -0800

    handle error

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -28,37 +28,36 @@ app.post('/sslCerts', async (req, res) => {
     message: 'SSL Certs and domain name records successfully created'
   }
 
-  try {
-    if (isProduction()) {
-      const sslCertResponse = await createSslCerts(
-        serviceResponse,
-        service,
-        selectedDomain
-      )
-      if (!sslCertResponse.success) return res.json(sslCertResponse)
+  if (isProduction()) {
+    await createSslCerts(serviceResponse, service, selectedDomain).catch(
+      error => {
+        console.error('sslCertResponse', error)
+        serviceResponse.success = false
+        serviceResponse.message = 'createSslCerts error'
+        return serviceResponse
+      }
+    )
 
-      const cnameResponse = await setCnameRecords(
-        service,
-        selectedDomain,
-        serviceResponse
-      )
-      if (!cnameResponse.success) return res.json(cnameResponse)
-    }
-
-    const domains = getAvailableDomains()
-    const domain: Domain = {
-      domain: selectedDomain,
-      expiration: '<ssl expiration date will go here>',
-      provider: service
-    }
-    domains.push(domain)
-    setData('availableDomains', domains)
-    return res.json(serviceResponse)
-  } catch (err) {
-    serviceResponse.success = false
-    serviceResponse.message = `Error: ${JSON.stringify(err)}`
-    return res.json(serviceResponse)
+    await setCnameRecords(service, selectedDomain, serviceResponse).catch(
+      error => {
+        console.error('cnameResponse', error)
+        serviceResponse.success = false
+        serviceResponse.message = 'setCnameRecords error'
+        return serviceResponse
+      }
+    )
   }
+
+  const domains = getAvailableDomains()
+  const domain: Domain = {
+    domain: selectedDomain,
+    expiration: '<ssl expiration date will go here>',
+    provider: service
+  }
+  domains.push(domain)
+  setData('availableDomains', domains)
+
+  return res.json(serviceResponse)
 })
 
 app.patch('/sslCerts/:selectedDomain', async (req, res) => {
