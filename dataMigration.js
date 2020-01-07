cdf8d41a1e2349b72be1305e0b5f673ff4c41fe3
hwong0305 <hwong.0305@gmail.com>
2020-01-06 22:13:01 -0800

    Corrected error with migration file

--- dataMigration.js
+++ dataMigration.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
 const file = fs.readFileSync('./data.db')
 const fileData = JSON.parse(file.toString() || '{}')
-fileData.mappings = fileData.mappings.map(string => string.toLowerCase())
+fileData.mappings = fileData.mappings.map(mapping => ({
+  ...mapping,
+  domain: mapping.domain.toLowerCase(),
+  subDomain: mapping.subDomain.toLowerCase(),
+  fullDomain: mapping.fullDomain.toLowerCase()
+}))
 const fileDataString = `${JSON.stringify(fileData, null, 2)}`
 fs.writeFile('./data.db', fileDataString, err => {
   if (err) {
