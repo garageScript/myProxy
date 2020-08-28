850a93d190d5a18e179aa58ab28acfedfdad9404
ggwadera <16023489+ggwadera@users.noreply.github.com>
2020-08-28 17:09:17 -0300

    fix to get correct user and group ids

--- src/helpers/getGitUser.ts
+++ src/helpers/getGitUser.ts
@@ -3,12 +3,12 @@ import cp from 'child_process'
 const exec = util.promisify(cp.exec)
 
 const getGitUserId = async (): Promise<number> => {
-  const gitId = await exec('id -u git')
+  const gitId = await exec('id -u myproxy')
   return parseInt(gitId.stdout, 10)
 }
 
 const getGitGroupId = async (): Promise<number> => {
-  const gitId = await exec('getent group git | cut -d: -f3')
+  const gitId = await exec('getent group myproxy | cut -d: -f3')
   return parseInt(gitId.stdout, 10)
 }
 
