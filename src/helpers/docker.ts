a71d38a503641c1f5a4b4dfefe93fd1fe769fd81
ggwadera <16023489+ggwadera@users.noreply.github.com>
2020-09-03 18:28:47 -0300

    helpers: docker helper functions

---
+++ src/helpers/docker.ts
@@ -0,0 +1,96 @@
+import Docker from 'dockerode'
+import path from 'path'
+import { Readable, PassThrough } from 'stream'
+import environment from '../helpers/environment'
+
+const docker = new Docker({ socketPath: '/var/run/docker.sock' })
+
+const getContainersList = async (): Promise<Docker.ContainerInfo[]> => {
+  const containers = await docker.listContainers({ all: true })
+  return containers
+}
+
+const getContainerLogs = async (
+  id: string,
+  options: Docker.ContainerLogsOptions
+): Promise<NodeJS.ReadableStream> => {
+  const container = docker.getContainer(id)
+  let logs = await container.logs(options)
+  if (!options.follow) {
+    // if follow = false, the logs are returned as a buffer
+    // so we need to convert into a stream
+    logs = Readable.from(logs)
+  }
+  const demuxedStream = new PassThrough()
+  container.modem.demuxStream(logs, demuxedStream, demuxedStream)
+  logs.on('end', () => demuxedStream.end())
+  return demuxedStream
+}
+
+const createContainer = async (
+  fullDomain: string,
+  port: number
+): Promise<string> => {
+  const workPath = path.resolve(environment.WORKPATH, fullDomain)
+  return docker
+    .createContainer({
+      Image: 'node:alpine',
+      name: fullDomain,
+      User: 'node',
+      ExposedPorts: {
+        '3000/tcp': {}
+      },
+      Tty: false,
+      WorkingDir: '/home/node/app',
+      Env: ['NODE_ENV=production', 'PORT=3000'],
+      HostConfig: {
+        Binds: [`${workPath}:/home/node/app`],
+        RestartPolicy: {
+          Name: 'on-failure',
+          MaximumRetryCount: 3
+        },
+        LogConfig: {
+          Type: 'json-file',
+          Config: {
+            'max-size': '10m',
+            'max-file': '1'
+          }
+        },
+        PortBindings: {
+          '3000/tcp': [
+            {
+              HostIp: '',
+              HostPort: port.toString()
+            }
+          ]
+        }
+      },
+      Cmd: ['node', '.']
+    })
+    .then(container => container.id)
+    .catch(err => err)
+}
+
+const startContainer = async (id: string): Promise<unknown> => {
+  const container = docker.getContainer(id)
+  return container.restart()
+}
+
+const stopContainer = async (id: string): Promise<unknown> => {
+  const container = docker.getContainer(id)
+  return container.stop()
+}
+
+const removeContainer = async (id: string): Promise<unknown> => {
+  const container = docker.getContainer(id)
+  return container.remove()
+}
+
+export {
+  getContainersList,
+  getContainerLogs,
+  createContainer,
+  startContainer,
+  stopContainer,
+  removeContainer
+}
