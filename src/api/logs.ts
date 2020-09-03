7fc7eeb7002506168b378ea4987994a5197a19f6
ggwadera <16023489+ggwadera@users.noreply.github.com>
2020-09-03 18:30:34 -0300

    api/logs: use single path with stream param
    Changed the api endpoint to use a single route, with a param for the stream (stdout or stderr)
    Removed the 'Clear Logs' feature, as Docker does not have a easy way to do that, but we can pass a 'tail=x' param in the URL.

--- src/api/logs.ts
+++ src/api/logs.ts
@@ -1,65 +1,37 @@
 import express from 'express'
-import fs from 'fs'
 import environment from '../helpers/environment'
 import { getMappingByDomain } from '../lib/data'
+import { getContainerLogs } from '../helpers/docker'
 
 const logsRouter = express.Router()
 const { isProduction } = environment
 
-logsRouter.get('/err/:domain', (req, res) => {
-  const { domain } = req.params
+logsRouter.get('/:stream/:domain', async (req, res) => {
+  const { stream, domain } = req.params
+  const { follow, tail } = req.query
+
+  // Stream validation
+  if (stream !== 'stdout' && stream !== 'stderr') {
+    return res
+      .status(400)
+      .json({ message: 'stream param must be stdout or stderr' })
+  }
 
   if (isProduction()) {
     // Only search for domain when running in production. The test does not
     // require a valid domain since it only verifies the endpoint
     const { fullDomain } = getMappingByDomain(domain)
-    // Pipes the error log files to res
+    // Pipes the log to res
     res.setHeader('content-type', 'text/plain')
-    fs.createReadStream(`/home/myproxy/.pm2/logs/${fullDomain}-err.log`).pipe(
-      res
-    )
+    const logStream = await getContainerLogs(fullDomain, {
+      follow: Boolean(follow),
+      tail: Number(tail),
+      [stream]: true
+    })
+    logStream.pipe(res)
   } else {
     res.send('OK')
   }
-})
-
-logsRouter.get('/out/:domain', (req, res) => {
-  const { domain } = req.params
-
-  if (isProduction()) {
-    // Only search for domain when running in production. The test does not
-    // require a valid domain since it only verifies the endpoint
-    const { fullDomain } = getMappingByDomain(domain)
-    // Pipes the output log file to res. Console.Log from your app will appear here
-    res.setHeader('content-type', 'text/plain')
-    fs.createReadStream(`/home/myproxy/.pm2/logs/${fullDomain}-out.log`).pipe(
-      res
-    )
-  } else {
-    res.send('OK')
-  }
-})
-
-logsRouter.delete('/:domain', (req, res) => {
-  const { domain } = req.params
-
-  if (isProduction())
-    fs.writeFile(
-      `/home/myproxy/.pm2/logs/${domain}-out.log`,
-      'Log cleared\n',
-      err => {
-        if (err) console.log('Error deleting output log')
-      }
-    )
-  fs.writeFile(
-    `/home/myproxy/.pm2/logs/${domain}-err.log`,
-    'Log cleared\n',
-    err => {
-      if (err) console.log('Error deleting error log')
-    }
-  )
-
-  res.send('LOGS DELETED')
 })
 
 export default logsRouter
